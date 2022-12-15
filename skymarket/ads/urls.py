6584620084488dad0dc5fa3adfb5cc4b5d634480
alstacon <alstacon@gmail.com>
2022-12-15 16:04:57 +0400

    rebase urls

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -1,9 +1,22 @@
 from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 
-# TODO настройка роутов для модели
+from ads.views import AdViewSet, CommentViewSet
+from rest_framework_nested.routers import NestedSimpleRouter
 
+router = SimpleRouter()
 
+router.register('ads', AdViewSet, basename='ads')
+
+comments_router = NestedSimpleRouter(
+    router,
+    'ads',
+    lookup='ad'
+)
+
+comments_router.register('comments', CommentViewSet, basename='ad_comments')
 
 urlpatterns = [
-
+    path("", include(router.urls)),
+    path("", include(comments_router.urls)),
 ]
